abc7971a375d41deaaeab629f6d3df11e2a1c7a9
Jay Schmidek <jay@schmidek.ca>
2010-11-29 20:00:17 -0700

    beautified some javascript

--- public/changes/resources/changes.js
+++ public/changes/resources/changes.js
@@ -1,31 +1,51 @@
-$(document).ready(function(){
-//initialize grid
-jQuery("#changes-table").jqGrid({        
-   	url:'changes.json',
-	datatype: "json",
-   	colNames:['Type','Movie','Diff','User','Time'],
-   	colModel:[
-   		{name:'change_type',index:'change_type', width:75},
-   		{name:'movie',index:'movies.name',width:200},
-   		{name:'diff',index:'diff',width:400},
-   		{name:'user',index:'users.login',width:100},
-   		{name:'created_at',index:'created_at',width:200}
-   	],
-   	rowNum:10,
-   	//rownumbers: true,
-   	pager: '#changes-div',
-   	sortname: 'created_at',
-    viewrecords: true,
-    sortorder: "desc",
-    //scroll:1,
-	caption: "Changes",
-	height: '100%',
-	//autowidth: true,
-	onSelectRow: function(id){
-		//self.handleClick(id);
-	}
-})
-.jqGrid('navGrid','#changes-div',{edit:false,del:false,add:false})
-.filterToolbar();
-$('.ui-jqgrid-titlebar').hide();
+$(document).ready(function() {
+	//initialize grid
+	jQuery("#changes-table").jqGrid({
+		url: 'changes.json',
+		datatype: "json",
+		colNames: ['Type', 'Movie', 'Diff', 'User', 'Time'],
+		colModel: [{
+			name: 'change_type',
+			index: 'change_type',
+			width: 75
+		},
+		{
+			name: 'movie',
+			index: 'movies.name',
+			width: 200
+		},
+		{
+			name: 'diff',
+			index: 'diff',
+			width: 400
+		},
+		{
+			name: 'user',
+			index: 'users.login',
+			width: 100
+		},
+		{
+			name: 'created_at',
+			index: 'created_at',
+			width: 200
+		}],
+		rowNum: 10,
+		//rownumbers: true,
+		pager: '#changes-div',
+		sortname: 'created_at',
+		viewrecords: true,
+		sortorder: "desc",
+		//scroll:1,
+		caption: "Changes",
+		height: '100%',
+		//autowidth: true,
+		onSelectRow: function( id ) {
+			//self.handleClick(id);
+		}
+	}).jqGrid('navGrid', '#changes-div', {
+		edit: false,
+		del: false,
+		add: false
+	}).filterToolbar();
+	$('.ui-jqgrid-titlebar').hide();
 });
--- public/movies/resources/movies.js
+++ public/movies/resources/movies.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function() {
 	new ListView();
 	new ItemView($("#itemview"));
 	new Controller();
@@ -6,73 +6,83 @@ $(document).ready(function(){
 	new AddMovie();
 });
 
-var AddMovie = function(){
+var AddMovie = function() {
 	var self = this;
-	
-	$(document).bind('ADD_MOVIE',function(e,data){ self.show(); });
-	
-	self.show = function(){
+
+	$(document).bind('ADD_MOVIE', function( e, data ) {
+		self.show();
+	});
+
+	self.show = function() {
 		div = "#addmovie_form";
-		
-		$("body").append($.View('//movies/views/addmovie.ejs',{}));
-		
+
+		$("body").append($.View('//movies/views/addmovie.ejs', {}));
+
 		$(div).dialog({
 			modal: true,
 			buttons: {
-				"Create": function(){
+				"Create": function() {
 					var name = $("#addmovie_name").val();
-					setTimeout(function(){ self.search(name); }, 10);
-					$( this ).dialog( "close" );
+					setTimeout(function() {
+						self.search(name);
+					}, 10);
+					$(this).dialog("close");
 				},
-				"Cancel": function(){
-					$( this ).dialog( "close" );
+				"Cancel": function() {
+					$(this).dialog("close");
 				}
 			},
-			close: function(){
+			close: function() {
 				$(div).remove();
 			}
 		});
 	};
-	
-	self.search = function(name){
-		$.getJSON('/tmdb/search?name='+name, self.showChoices);
+
+	self.search = function( name ) {
+		$.getJSON('/tmdb/search?name=' + name, self.showChoices);
 	};
-	
-	self.showChoices = function(movies){
-		
+
+	self.showChoices = function( movies ) {
+
 		div = "#choices";
-		
-		$("body").append($.View('//movies/views/selection.ejs',{movies: movies}));
-		$(".choice").click(function(e){
+
+		$("body").append($.View('//movies/views/selection.ejs', {
+			movies: movies
+		}));
+		$(".choice").click(function( e ) {
 			var id = $(this).val();
-			setTimeout(function(){ self.getInfo(id); }, 10);
+			setTimeout(function() {
+				self.getInfo(id);
+			}, 10);
 			$(div).dialog("close");
 		});
-		
+
 		$(div).dialog({
 			modal: true,
 			buttons: {
-				"Cancel": function(){
-					$( this ).dialog( "close" );
+				"Cancel": function() {
+					$(this).dialog("close");
 				}
 			},
-			close: function(){
+			close: function() {
 				$(div).remove();
 			}
 		});
 	};
-	
-	self.getInfo = function(id){
-		$.getJSON('/tmdb/getInfo?id='+id, function(data){self.addMovies(data[0]);});
+
+	self.getInfo = function( id ) {
+		$.getJSON('/tmdb/getInfo?id=' + id, function( data ) {
+			self.addMovies(data[0]);
+		});
 	};
-	
-	self.addMovies = function(info){
+
+	self.addMovies = function( info ) {
 		var movie = new Object();
 		movie.name = info.name;
 		movie.added = new Date();
-		for(var i in info.posters){
+		for ( var i in info.posters ) {
 			var poster = info.posters[i].image;
-			if(poster.size == "cover"){
+			if ( poster.size == "cover" ) {
 				movie.cover = poster.url;
 				break;
 			}
@@ -82,178 +92,220 @@ var AddMovie = function(){
 		movie.imdb = info.imdb_id;
 		movie.trailer = info.trailer;
 		var genres = new Array();
-		for(var i in info.genres){
+		for ( var i in info.genres ) {
 			genres.push(info.genres[i].name);
 		}
 		movie.genres = genres;
-		$(document).trigger('CREATE_MOVIE',movie);
+		$(document).trigger('CREATE_MOVIE', movie);
 	};
-	
+
 };
 
-var ListView = function(){
-	
+var ListView = function() {
+
 	var self = this;
-	
+
 	//initialize grid
-	jQuery("#moviegrid").jqGrid({        
-	   	url:'movies/grid.json',
+	jQuery("#moviegrid").jqGrid({
+		url: 'movies/grid.json',
 		datatype: "json",
-	   	colNames:['Name','Year','Date Added'],
-	   	colModel:[
-	   		{name:'name',index:'name', width:350},
-	   		{name:'year',index:'year',width:100},
-	   		{name:'added',index:'added',width:100}	
-	   	],
-	   	rowNum:20,
-	   	rownumbers: true,
-	   	pager: '#movienav',
-	   	sortname: 'added',
-	    viewrecords: true,
-	    sortorder: "desc",
-	    //scroll:1,
+		colNames: ['Name', 'Year', 'Date Added'],
+		colModel: [{
+			name: 'name',
+			index: 'name',
+			width: 350
+		},
+		{
+			name: 'year',
+			index: 'year',
+			width: 100
+		},
+		{
+			name: 'added',
+			index: 'added',
+			width: 100
+		}],
+		rowNum: 20,
+		rownumbers: true,
+		pager: '#movienav',
+		sortname: 'added',
+		viewrecords: true,
+		sortorder: "desc",
+		//scroll:1,
 		caption: "Movies",
 		height: '100%',
 		//autowidth: true,
-		onSelectRow: function(id){
+		onSelectRow: function( id ) {
 			self.handleClick(id);
 		},
-		loadComplete: function(){
+		loadComplete: function() {
 			$("#moviegrid").find("tbody tr:nth-child(2)").click();
 		}
-	})
-	.jqGrid('navGrid','#movienav',{edit:false,del:false,addtext:"Add Movie",addfunc:function(){ $(document).trigger('ADD_MOVIE'); } })
-	.filterToolbar();
+	}).jqGrid('navGrid', '#movienav', {
+		edit: false,
+		del: false,
+		addtext: "Add Movie",
+		addfunc: function() {
+			$(document).trigger('ADD_MOVIE');
+		}
+	}).filterToolbar();
 	$('.ui-jqgrid-titlebar').hide();
-	
-	$(window).bind('resize', function(){
-		$("#moviegrid").setGridWidth($("#listview").width()-20, true);
+
+	$(window).bind('resize', function() {
+		$("#moviegrid").setGridWidth($("#listview").width() - 20, true);
 	}).trigger('resize');
-	
+
 	self.currentId = -1;
-	self.handleClick = function(id){
-		if(self.currentId != id){
+	self.handleClick = function( id ) {
+		if ( self.currentId != id ) {
 			self.currentId = id;
-			$(document).trigger('MOVIE_SELECTED',id);
+			$(document).trigger('MOVIE_SELECTED', id);
 		}
 	};
-	
+
 };
 
-var ItemView = function($div){
-	
+var ItemView = function( $div ) {
+
 	var self = this;
-	
+
 	var div = $div;
-		
-	$(document).bind('CURRENT_MOVIE_CHANGED',function(e,data){ self.updateCurrent(data); });
-	$(document).bind('CREATE_MOVIE',function(e,data){ self.pendingCreate(data); });
-	
-	self.updateCurrent = function(movie){
-		div.html($.View('//movies/views/movie.ejs',{ movie: movie }));
+
+	$(document).bind('CURRENT_MOVIE_CHANGED', function( e, data ) {
+		self.updateCurrent(data);
+	});
+	$(document).bind('CREATE_MOVIE', function( e, data ) {
+		self.pendingCreate(data);
+	});
+
+	self.updateCurrent = function( movie ) {
+		div.html($.View('//movies/views/movie.ejs', {
+			movie: movie
+		}));
 		div.find("button").button();
-		$("#rating").raty({ readOnly: true, start: movie.rating, number:10, path: '/images/' });
-		$("#editMovie").click(function(){
+		$("#rating").raty({
+			readOnly: true,
+			start: movie.rating,
+			number: 10,
+			path: '/images/'
+		});
+		$("#editMovie").click(function() {
 			self.editCurrent(movie);
 		});
 	};
-	
-	self.editCurrent = function(movie){
-		div.html($.View('//movies/views/editmovie.ejs',{ movie: movie }));
+
+	self.editCurrent = function( movie ) {
+		div.html($.View('//movies/views/editmovie.ejs', {
+			movie: movie
+		}));
 		div.find("button,input[type=submit]").button();
-		$("#rating").raty({ start: movie.rating, number:10, path: '/images/', scoreName: 'movie[rating]', showCancel: true });
-		$("#formatset",div).buttonset();
-		$("#cancel").click(function(){
+		$("#rating").raty({
+			start: movie.rating,
+			number: 10,
+			path: '/images/',
+			scoreName: 'movie[rating]',
+			showCancel: true
+		});
+		$("#formatset", div).buttonset();
+		$("#cancel").click(function() {
 			self.updateCurrent(movie);
 		});
-		$("#movieForm").submit(function(){
+		$("#movieForm").submit(function() {
 			var $form = $("#movieForm");
 			$.post(
-				$form.attr("action") + ".json",
-				$form.serialize() + "&_method=put",
-				function(data){
-					//alert(data.success);
-				}
-			);
-			$(document).trigger('MOVIE_UPDATED',movie.id);
+			$form.attr("action") + ".json", $form.serialize() + "&_method=put", function( data ) {
+				//alert(data.success);
+			});
+			$(document).trigger('MOVIE_UPDATED', movie.id);
 			return false;
 		});
 	};
-	
-	self.pendingCreate = function(movie){
-		div.html($.View('//movies/views/createmovie.ejs',{ movie: movie }));
+
+	self.pendingCreate = function( movie ) {
+		div.html($.View('//movies/views/createmovie.ejs', {
+			movie: movie
+		}));
 		div.find("button,input[type=submit]").button();
-		$("#rating").raty({ number:10, path: '/images/', scoreName: 'movie[rating]' });
-		$("#formatset",div).buttonset();
-		$("#cancel").click(function(){
+		$("#rating").raty({
+			number: 10,
+			path: '/images/',
+			scoreName: 'movie[rating]'
+		});
+		$("#formatset", div).buttonset();
+		$("#cancel").click(function() {
 			self.updateCurrent(movie);
 		});
-		$("#movieForm").submit(function(){
+		$("#movieForm").submit(function() {
 			var $form = $("#movieForm");
 			$.post(
-				$form.attr("action") + ".json",
-				$form.serialize() + "&_method=post",
-				function(data){
-					//alert(data.success);
-				}
-			);
+			$form.attr("action") + ".json", $form.serialize() + "&_method=post", function( data ) {
+				//alert(data.success);
+			});
 			$(document).trigger('NEW_MOVIE');
 			return false;
 		});
 	};
-	
+
 };
 
-var Controller = function(){
-	
+var Controller = function() {
+
 	var self = this;
-	
-	$(document).bind('MOVIE_SELECTED',function(e,data){ self.updateSelected(data); });
-	$(document).bind('MOVIE_RETRIEVED',function(e,data){ self.retrieved(data); });
-	
-	self.updateSelected = function(id){
-		$(document).trigger('GET_MOVIE',id);
+
+	$(document).bind('MOVIE_SELECTED', function( e, data ) {
+		self.updateSelected(data);
+	});
+	$(document).bind('MOVIE_RETRIEVED', function( e, data ) {
+		self.retrieved(data);
+	});
+
+	self.updateSelected = function( id ) {
+		$(document).trigger('GET_MOVIE', id);
 	};
-	
-	self.retrieved = function(movie){
-		$(document).trigger('CURRENT_MOVIE_CHANGED',movie);
+
+	self.retrieved = function( movie ) {
+		$(document).trigger('CURRENT_MOVIE_CHANGED', movie);
 	};
-	
-	$(document).bind('MOVIE_UPDATED',function(e,id){
+
+	$(document).bind('MOVIE_UPDATED', function( e, id ) {
 		//$("#moviegrid").trigger("reloadGrid"); 
-		$(document).trigger('INVALIDATE_CACHE',id);
+		$(document).trigger('INVALIDATE_CACHE', id);
 		self.updateSelected(id);
 	});
-	
-	$(document).bind('NEW_MOVIE',function(e){
-		$("#moviegrid").trigger("reloadGrid"); 
+
+	$(document).bind('NEW_MOVIE', function( e ) {
+		$("#moviegrid").trigger("reloadGrid");
 	});
-	
+
 };
 
-var Model = function(){
-	
+var Model = function() {
+
 	var self = this;
 	var cache = new Object();
-	
-	$(document).bind('GET_MOVIE',function(e,data){ self.getMovie(data); });
-	
-	self.getMovie = function(id){
+
+	$(document).bind('GET_MOVIE', function( e, data ) {
+		self.getMovie(data);
+	});
+
+	self.getMovie = function( id ) {
 		//Check if movie is in cache
-		if(cache[id]){
+		if ( cache[id] ) {
 			//Cache hit
 			var movie = cache[id];
-			$(document).trigger('MOVIE_RETRIEVED',movie);
-		}else{
+			$(document).trigger('MOVIE_RETRIEVED', movie);
+		} else {
 			//Get Movie from server
-			$.getJSON('movies/'+id+'.json',function(data){
+			$.getJSON('movies/' + id + '.json', function( data ) {
 				var movie = data.movie;
 				cache[id] = movie;
-				$(document).trigger('MOVIE_RETRIEVED',movie);
+				$(document).trigger('MOVIE_RETRIEVED', movie);
 			});
 		}
 	};
-	
-	$(document).bind('INVALIDATE_CACHE',function(e,id){ delete cache[id]; });
-	
+
+	$(document).bind('INVALIDATE_CACHE', function( e, id ) {
+		delete cache[id];
+	});
+
 };
