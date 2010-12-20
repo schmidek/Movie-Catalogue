72d61bb3dcc0f2766d0adde6b58638c9d12623ab
Jay Schmidek <jay@schmidek.ca>
2010-12-20 12:09:04 -0700

    css transition experimentation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,7 +35,9 @@ class ApplicationController < ActionController::Base
     end
     
     def store_location
-      session[:return_to] = request.request_uri
+      unless request.request_uri == '/'
+        session[:return_to] = request.request_uri
+      end
     end
     
     def redirect_back_or_default(default)
--- config/routes.rb
+++ config/routes.rb
@@ -22,52 +22,10 @@ Site::Application.routes.draw do
 		end
 	end
   end
-  #match :account, :controller => "users"
 
-  root :to => "user_sessions#new"
+  root :to => "users#show"
   match "logout" => "user_sessions#destroy"
   match "login" => "user_sessions#new"
-
-
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
 
   # Sample resource route within a namespace:
   #   namespace :admin do
@@ -76,14 +34,5 @@ Site::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
 
 end
